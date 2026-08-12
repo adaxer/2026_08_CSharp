5b86f0c60745c598f17b7d40c20361c29851a267
Andreas Daxer <info@andreas-daxer.de>
2026-08-12 17:01:13 +0200

    Modul7

---
+++ Projekte/Lab05/Lab05/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace Lab05;
+
+public class Program
+{
+    public Program()
+    {
+        this.Id = ++InstanceCount;
+    }
+
+    public int Id { get; private init; }
+
+    public static int InstanceCount { get; private set; } = 0;
+
+    private static void Main(string[] args)
+    {
+        bool isDone = false;
+        double number1 = 0;
+        double number2 = 0;
+        double result = 0;
+        Operation operation = Operation.Add;
+
+        var p = new Program();
+        p.DoStuff();
+        Program.GetNumber("");
+        
+        //p.Id = 0;
+        //Program.InstanceCount = 0;
+
+        while (!isDone)
+        {
+            // Console.Clear();
+            number1 = GetNumber("Zahl 1 eingeben: ");
+            number2 = GetNumber("Zahl 2 eingeben: ");
+
+            operation = GetOperation("Operation auswählen:\n1. Add\n2. Subtract\n3. Multiply\n4. Divide");
+
+            result = DoCalculation(number1, number2, operation);
+
+            Console.WriteLine($"Operation {operation} angewandt auf {number1} und {number2} ergibt: {result}");
+            Console.WriteLine("Nochmal (J/N)?");
+            string key = Console.ReadLine();
+            if (key.ToUpper() != "J")
+            {
+                isDone = true;
+            }
+        }
+    }
+
+    private static Operation GetOperation(string message)
+    {
+        Console.WriteLine(message);
+        Operation result = Operation.Add;
+        while (!Enum.TryParse<Operation>(Console.ReadLine(), out result) || !Enum.GetValues<Operation>().Contains(result))// ((int)result<1) || (int)result>4)
+        {
+            Console.WriteLine("Keine Operation - Bitte nochmal.");
+        }
+        return result;
+    }
+
+    private static double DoCalculation(double number1, double number2, Operation operation)
+    {
+        switch (operation)
+        {
+            case Operation.Add:
+                return number1 + number2;
+            case Operation.Subtract:
+                return number1 - number2;
+            case Operation.Multiply:
+                return number1 * number2;
+            case Operation.Divide:
+                return number1 / number2;
+            default:
+                Console.WriteLine("Keine Operation gefunden");
+                return 0;
+        }
+    }
+
+    private static double GetNumber(string message)
+    {
+        Console.Write(message);
+        double result = 0;
+        while (!double.TryParse(Console.ReadLine(), out result))
+        {
+            Console.WriteLine("Keine Kommazahl - Bitte nochmal.");
+        }
+        return result;
+    }
+
+    public void DoStuff()
+    {
+        Console.WriteLine($"Program {this.Id} doing stuff");
+    }
+}
+
+// Speicher
+// Program 8Bytes, Id(Wert1):4 Bytes
+// Program 8Bytes, Id(Wert2):4 Bytes
+
+
+enum Operation
+{
+    Add = 1,
+    Subtract,
+    Multiply,
+    Divide
+}
+
---
+++ Projekte/Modul06-08/Modul06-08/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Modul06_08;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        PointStruct? nullablePointStruct = null;
+        Nullable<PointStruct> theSameThing=null;
+
+        PointStruct emptyPoint;
+        // emptyPoint.X++; Darf man nicht verwenden wenn nicht zugewiesen
+        PointStruct p = new PointStruct(3, 4);
+        ChangeX(p);
+        ChangePoint(ref p);
+        ChangeStructProperty(p);
+        // p = null; // Not allowed for structs
+
+        PointClass emptyPointClass;
+        PointClass pc = new PointClass(1, 2);
+        ChangeInstanceProperty(pc);
+        pc = null;
+    }
+
+    private static void ChangeInstanceProperty(PointClass pc)
+    {
+        pc.X++;
+        pc.Y++;
+    }
+
+    private static void ChangeStructProperty(PointStruct p)
+    {
+        p.X += 1;
+        p.Y += 1;
+    }
+
+    private static void ChangePoint(ref PointStruct p)
+    {
+        p=new PointStruct(p.X+1, p.Y+1);
+    }
+
+    private static void ChangeX(PointStruct p)
+    {
+        p = new PointStruct(p.X + 1, p.Y + 1);
+    }
+}
+
+public struct PointStruct
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public PointStruct(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+    public override string ToString()
+    {
+        return $"({X}, {Y})";
+    }
+}
+public class PointClass
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public PointClass(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+    public override string ToString()
+    {
+        return $"({X}, {Y})";
+    }
+}
+
+public class BusinessObject
+{
+    public int? Id { get; set; } = null;
+
+    // Andere Properties
+}
