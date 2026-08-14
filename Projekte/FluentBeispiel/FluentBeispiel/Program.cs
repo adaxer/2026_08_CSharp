db4f85fb238ff14d8378b65e3f1a21c7afc62331
Andreas Daxer <info@andreas-daxer.de>
2026-08-14 10:07:57 +0200

    Fluent-Beispiel, Lab11 vorbereitet

---
+++ Projekte/FluentBeispiel/FluentBeispiel/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace FluentBeispiel;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("log.txt")
+            .CreateLogger();
+    }
+}
+
+public class LoggerConfiguration
+{
+    internal object CreateLogger()
+    {
+        return new Logger();
+    }
+
+    public Targets WriteTo => new Targets();
+}
+
+public class Targets
+{
+    public LoggerConfiguration Console()
+    {
+        // Merkt sich dass Console verwendet werden soll
+        return new LoggerConfiguration();
+    }
+
+    public LoggerConfiguration File(string path)
+    {
+        // Merkt sich dass File verwendet werden soll
+        return new LoggerConfiguration();
+    }
+}
+
+public class Logger
+{
+    
+}
---
+++ Projekte/Lab11/Lab11.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab11.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        
+    }
+}
